a435d49190d8534b56d5d4078588891f302d1a41
liuyi <me@liuyi.io>
2024-12-25 17:10:16 +0800

    init

---
+++ bash/.bashrc.d/bash_aliases.sh
@@ -0,0 +1,4 @@
+alias ll="ls -l"
+alias la="ls -la"
+
+alias nlazy="NVIM_APPNAME=LazyVim nvim"
---
+++ bash/.bashrc.d/env.sh
@@ -0,0 +1,7 @@
+
+export GTK_IM_MODULE=ibus
+export QT_IM_MODULE=ibus
+export XMODIFIERS=@im=ibus
+
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
+export QT_ENABLE_HIGHDPI_SCALING=1
